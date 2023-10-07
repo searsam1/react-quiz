6de3c092176766463c766ca0ffa3821f139eabfa
Alec Sears <amsears60441@gmail.com>
2023-10-07 14:41:12 -0600

    add content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { questions } from './questions';
 import './App.css';
 
 function App() {
+  const [questionIdx, setQuestionIdx] = useState(0);
+  const [answer, setAnswer] = useState('');
+  const [score, setScore] = useState(0);
+
+  const checkAnswer = () => {
+    const correctAnswer = questions[questionIdx].correctAnswer;
+    return answer === correctAnswer && !questions[questionIdx].attempted;
+  };
+
+  const updateScore = () => {
+    setScore(score + 1);
+    questions[questionIdx].attempted = true;
+  };
+
+  const nextQuestion = () => {
+    if (checkAnswer()) {
+      updateScore();
+    }
+    setAnswer('');
+    setQuestionIdx(questionIdx + 1);
+
+    if (questionIdx === questions.length - 1) {
+      alert(score);
+      setQuestionIdx(0);
+      setScore(0);
+    }
+  };
+
+  const previousQuestion = () => {
+    setQuestionIdx(questionIdx - 1);
+  };
+
+  const currentQuestion = questions[questionIdx];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='Page'>
+      <div className="Quiz">
+        <h2>{currentQuestion.question} ( {questionIdx + 1} )</h2>
+        <form>
+          {currentQuestion.answers.map((ans, idx) => (
+            <div key={idx}>
+              <input
+                type="radio"
+                id={`answer${idx}`}
+                name="answer"
+                value={ans}
+                onChange={({ target }) => setAnswer(target.value)}
+                checked={answer === ans}
+              />
+              <label htmlFor={`answer${idx}`}>{ans}</label>
+            </div>
+          ))}
+        </form>
+        <button disabled={questionIdx === 0} onClick={previousQuestion}>
+          Previous
+        </button>
+        <button disabled={questionIdx === questions.length - 1} onClick={nextQuestion}>
+          Next
+        </button>
+        <p className='score'>
+          Score: <span>{score}</span> / {questions.length}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,52 @@
+export const questions = [
+  {
+    question: "What is React?",
+    answers: ["A JavaScript library", "A programming language", "A CSS framework", "A code editor"],
+    correctAnswer: "A JavaScript library"
+  },
+  {
+    question: "What is JSX?",
+    answers: ["A JavaScript library", "A syntax extension for JavaScript", "A CSS preprocessor", "A JavaScript framework"],
+    correctAnswer: "A syntax extension for JavaScript"
+  },
+  {
+    question: "What is a React component?",
+    answers: ["A function that returns a piece of HTML", "A CSS class", "A JavaScript library", "A programming language"],
+    correctAnswer: "A function that returns a piece of HTML"
+  },
+  {
+    question: "How do you create a React component?",
+    answers: ["Using the `React.create()` function", "Using the `createComponent()` function", "Using the `React.Component` class", "Using the `createElement()` function"],
+    correctAnswer: "Using the `React.Component` class"
+  },
+  {
+    question: "What is a React Hook?",
+    answers: ["A CSS class", "A JavaScript function", "A JavaScript class", "A React component"],
+    correctAnswer: "A JavaScript function"
+  },
+  {
+    question: "What is the purpose of the `useState` hook in React?",
+    answers: ["To create a class component", "To create a state variable in a function component", "To update a prop in a function component", "To create a new React element"],
+    correctAnswer: "To create a state variable in a function component"
+  },
+  {
+    question: "What is the purpose of the `useEffect` hook in React?",
+    answers: ["To add an event listener to a DOM element", "To perform side effects in a function component", "To update a state variable in a function component", "To create a new React component"],
+    correctAnswer: "To perform side effects in a function component"
+  },
+  {
+    question: "How do you pass data from a parent component to a child component in React?",
+    answers: ["Using props", "Using state", "Using a context", "Using a ref"],
+    correctAnswer: "Using props"
+  },
+  {
+    question: "What is the purpose of the `key` prop in React?",
+    answers: ["To uniquely identify an element in a list", "To set the CSS `key` property of an element", "To create a new React component", "To add an event listener to a DOM element"],
+    correctAnswer: "To uniquely identify an element in a list"
+  },
+  {
+    question: "How do you conditionally render a component in React?",
+    answers: ["Using the `if` statement", "Using the `switch` statement", "Using the `? :` ternary operator", "Using the `&&` operator"],
+    correctAnswer: "Using the `? :` ternary operator"
+  }
+];
